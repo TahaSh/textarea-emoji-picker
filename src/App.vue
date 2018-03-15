243a4693593be94c96d73cbefe285260af34c726
Taha Shashtari <taha.shashtari@gmail.com>
2018-03-15 19:12:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home-page">
+    <textarea-emoji-picker
+      v-model="text"
+    />
+  </div>
+</template>
+
+<script>
+import TextareaEmojiPicker from './TextareaEmojiPicker'
+
+export default {
+  name: 'HomePage',
+  components: { TextareaEmojiPicker },
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home-page {
+  padding-top: 50px;
+}
+</style>
